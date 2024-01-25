e54bcd4f93c09c81d4c1856b78cebad80b45ad28
Iryna_Ptashka <bird.forest70@gmail.com>
2024-01-25 18:57:23 +0200

    fixed1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { persistor, store } from 'redux/store';
+import { persistor, store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'App';
