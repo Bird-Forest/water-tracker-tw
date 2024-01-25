57a919e10aa79681ca009223b8891a82340598e8
Iryna_Ptashka <bird.forest70@gmail.com>
2024-01-25 18:36:13 +0200

    added redax

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 export const App = () => {
   return (
+    // це час в мілісулундах
+
     <div
       style={{
         height: '100vh',
@@ -7,7 +9,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { Provider } from 'react-redux';
+import { persistor, store } from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { App } from 'App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="goit-react-hw-08-phonebook">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
