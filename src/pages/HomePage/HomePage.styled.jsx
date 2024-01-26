89acb507f579bfa02e13efbbb6cfdf9fb430c6c1
Ihor_Koniaha <konyaga.igor10@gmail.com>
2024-01-26 15:46:58 +0200

    add background

--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -1,10 +1,26 @@
 import styled from 'styled-components';
+import BackgroundDesktopDrops from '../../img/BackgroundDesktopDrops.png';
 
 export const StyledHomePage = styled.section`
   display: flex;
   column-gap: 32px;
 
   padding: 20px 0 40px 0;
+  position: relative;
+
+  &::before {
+    content: '';
+    display: block;
+    width: 1380px;
+    height: 570px;
+    position: absolute;
+    top: 49px;
+    left: -76px;
+
+    background-image: url(${BackgroundDesktopDrops});
+    background-repeat: no-repeat;
+    background-size: 1380px 570px;
+  }
 
   .wrapper {
     padding: 30px 0 16px 0;
--- src/pages/Pages.styled.js
+++ src/pages/Pages.styled.js
@@ -5,10 +5,10 @@ import MainPageDecktop from '../img/MainPageDecktop.png';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
+  max-width: 1246px;
   height: 100%;
   margin: 0 auto;
-  padding: 0;
+  padding: 0 15px;
 `;
 export const Header = styled.header`
   display: flex;
