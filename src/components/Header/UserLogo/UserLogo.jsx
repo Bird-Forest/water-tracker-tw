16118e7a2116e1d2ce89718c2b28d72ba570484e
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2024-01-27 15:03:57 +0200

    part header

--- src/components/Header/UserLogo/UserLogo.jsx
+++ src/components/Header/UserLogo/UserLogo.jsx
@@ -1,2 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { UseSelector } from 'react-redux';
+import { selectUser } from '../../../redux/auth/selectors';
+import {
+  UserAvatar,
+  UserName,
+  UserLogoBtn,
+  UserLogoText,
+  UserLogoIcon,
+  UserLogoContainer,
+} from './UserLogo.styled';
+import sprite from '../../../img/sprite.svg';
