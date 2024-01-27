7115c64641f97f5d5b0a57a1d3430101e838d500
Iryna_Ptashka <bird.forest70@gmail.com>
2024-01-27 07:53:36 +0200

    fixed home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,41 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-// export default function HomePage() {
-//   return <div>HomePage</div>;
-// }
-
-import DailyNormalModal from 'components/DailyNormalModal/DailyNormalModal.jsx';
-import {
-  MyDailyNormaDiv,
-  MyDailyNorma,
-  Volume,
-  Edit,
-  Box,
-} from './HomePage.styled.js';
-
-export const HomePage = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  // const toggleModal = () => setShowModal(!showModal);
-  const toggleModal = () => {
-    setShowModal(!showModal);
-    console.log(showModal);
-  };
-
-  return (
-    <>
-      <div>HomePage</div>
-
-      <MyDailyNormaDiv>
-        <MyDailyNorma>My daily norma</MyDailyNorma>
-        <Box>
-          <Volume>1.5 L</Volume>
-
-          <Edit onClick={toggleModal}>Edit</Edit>
-        </Box>
-      </MyDailyNormaDiv>
-
-      {showModal && <DailyNormalModal closeModal={toggleModal} />}
-    </>
-  );
-};
+export default function HomePage() {
+  return <div>HomePage</div>;
+}
