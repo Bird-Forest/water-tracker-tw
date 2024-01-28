fcaca70d33cfde39255e71f12b7373a70c6cb105
Iryna_Ptashka <bird.forest70@gmail.com>
2024-01-29 00:30:57 +0200

    fixed heder

--- src/pages/AppBar.jsx
+++ src/pages/AppBar.jsx
@@ -1,6 +1,5 @@
-import NavigatePages from 'components/Header/NavigatePages';
 import React from 'react';
-import { WrapHeader } from './Pages.styled';
+
 import HeaderComponent from 'components/Header/Header';
 
 export default function AppBar() {
