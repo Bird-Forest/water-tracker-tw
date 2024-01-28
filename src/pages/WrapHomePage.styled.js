f01f6afdab99395cbd3bcfb19095b8fe7f29cc99
Ihor_Koniaha <konyaga.igor10@gmail.com>
2024-01-28 20:29:03 +0200

    update background home page

--- src/pages/WrapHomePage.styled.js
+++ src/pages/WrapHomePage.styled.js
@@ -5,19 +5,22 @@ export const StyledHomePage = styled.section`
   flex-direction: column;
   row-gap: 40px;
 
-  padding: 0 20px;
+  padding: 24px 20px 0;
   margin: 0 auto;
 
   width: 320px;
 
   @media only screen and (min-width: 768px) {
     width: 746px;
+    padding-top: 40px;
   }
 
   @media only screen and (min-width: 1440px) {
     flex-direction: row;
     row-gap: 0;
     column-gap: 32px;
+    padding-top: 20px;
+
     width: 1256px;
   }
 `;
