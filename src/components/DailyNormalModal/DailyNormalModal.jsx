ae9be75ed6788dad6c9a98a3e0ecdc64d186aef3
Roman <jrj80mail@gmail.com>
2024-01-29 13:54:29 +0200

    up DailyNormalModal (+local State & calculateWaterVol)
    "user input for gender, weight, active training hours, and volume-related goals in the handleSave function. Introduced state variables using useState and implemented a function (calculateWaterVol) to calculate the daily water volume"

--- src/components/DailyNormalModal/DailyNormalModal.jsx
+++ src/components/DailyNormalModal/DailyNormalModal.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import Modal from 'components/Modal/Modal';
+import { useState } from 'react';
+// import Modal from 'components/Modal/Modal';
+import { ModalWindow } from '../ModalWindow/ModalWindow';
 
 import {
   BoxTextNote,
@@ -8,12 +9,43 @@ import {
   BlueText,
   NoteText,
   DailyNormWrap,
+  CalcBox,
+  BoxForm,
+  LabelGender,
 } from './DailyModal.styled';
 
 const DailyNormalModal = ({ closeModal }) => {
+  const [gender, setGender] = useState('girl');
+  const [weight, setWeight] = useState('');
+  const [activeTraningHours, setActiveTraningHours] = useState('');
+  const [dailyVol, setDailyVol] = useState('1.5');
+  const [volGoal, setVolGoal] = useState('');
+
+  const calculateWaterVol = () => {
+    const factor = gender === 'girl' ? 0.03 : 0.04;
+    const activityFactor = gender === 'girl' ? 0.4 : 0.6;
+    const vol = (weight * factor + activeTraningHours * activityFactor).toFixed(
+      1
+    );
+    setDailyVol(vol);
+  };
+
+  const handleSave = async () => {
+    calculateWaterVol();
+
+    //! *temporary entry to avoid the error message:*
+    // eslint-disable-next-line no-unused-vars
+    const userData = {
+      gender,
+      weight,
+      activeTraningHours,
+      dailyVol,
+    };
+  };
+
   return (
     <DailyNormWrap>
-      <Modal closeModal={closeModal}>
+      <ModalWindow closeModal={closeModal}>
         <Title>My daily norma</Title>
 
         <Gender>
@@ -23,7 +55,7 @@ const DailyNormalModal = ({ closeModal }) => {
 
         <Gender>
           For man:
-          <BlueText>V=(M*0,03) + (T*0,4)</BlueText>
+          <BlueText>V=(M*0,04) + (T*0,6)</BlueText>
         </Gender>
 
         <BoxTextNote>
@@ -35,7 +67,75 @@ const DailyNormalModal = ({ closeModal }) => {
             set 0)
           </NoteText>
         </BoxTextNote>
-      </Modal>
+
+        <CalcBox>
+          <form>
+            <BoxForm>
+              <h4>Calculate your rate:</h4>
+              <LabelGender>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="girl"
+                  checked={gender === 'girl'}
+                  onChange={() => setGender('girl')}
+                />
+                For girl
+              </LabelGender>
+              <LabelGender>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="man"
+                  checked={gender === 'man'}
+                  onChange={() => setGender('man')}
+                />
+                For boy
+              </LabelGender>
+            </BoxForm>
+
+            <BoxForm>
+              <p>Your weight in kilograms:</p>
+              <input
+                type="number"
+                placeholder="kg"
+                value={weight}
+                onChange={e => setWeight(e.target.value)}
+              />
+            </BoxForm>
+
+            <BoxForm>
+              <p>
+                Time of active participation in sports or other activities with
+                a high physical load:
+              </p>
+              <input
+                type="number"
+                placeholder="Time in hours"
+                value={activeTraningHours}
+                onChange={e => setActiveTraningHours(e.target.value)}
+              />
+            </BoxForm>
+
+            <BoxForm>
+              The required amount of water in liters per day:{' '}
+              <strong>{dailyVol} L</strong>
+            </BoxForm>
+
+            <BoxForm>
+              <p>Write down how much water you will drink:</p>
+              <input
+                type="number"
+                placeholder="L"
+                value={volGoal}
+                onChange={e => setVolGoal(e.target.value)}
+              />
+            </BoxForm>
+
+            <button onClick={handleSave}>Save</button>
+          </form>
+        </CalcBox>
+      </ModalWindow>
     </DailyNormWrap>
   );
 };
