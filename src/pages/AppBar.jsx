3acc420f46992a421e2ff83dcb1c25db87d0de75
eddvornichenko <127417581+eddvornichenko@users.noreply.github.com>
2024-01-30 10:15:37 +0200

    part

--- src/pages/AppBar.jsx
+++ src/pages/AppBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import HeaderComponent from 'components/Header/Header';
+import { WrapHeader } from './Pages.styled';
 
 export default function AppBar() {
   return (
-    // <WrapHeader>
-    <HeaderComponent />
-    // </WrapHeader>
+    <WrapHeader>
+      <HeaderComponent />
+    </WrapHeader>
   );
 }
