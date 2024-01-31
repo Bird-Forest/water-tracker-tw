394679f6e0c5cbd41c4e64c8138c7afbdd937abc
Motkova Kateryna <sirenkoekaterina90@gmail.com>
2024-01-31 11:50:04 +0200

    TodayWaterList
    added components SectionTodayMonth and TodayWaterList

---
+++ src/components/SectionTodayMonth/SectionTodayMonth.jsx
@@ -0,0 +1,10 @@
+import { StyledSectionTodayMonth } from './SectionTodayMonth.styled';
+import TodayWaterList from './TodayWaterList/TodayWaterList';
+
+export const SectionTodayMonth = () => {
+  return (
+    <StyledSectionTodayMonth>
+      <TodayWaterList />
+    </StyledSectionTodayMonth>
+  );
+};
