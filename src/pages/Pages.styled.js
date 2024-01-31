f8ce24c5f94a8512d081b8982a441a5c669da1ba
Iryna_Ptashka <bird.forest70@gmail.com>
2024-01-31 08:37:39 +0200

    create data, delete WrapModal, added file img

--- src/pages/Pages.styled.js
+++ src/pages/Pages.styled.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
-// const MainPageDecktop = '../img/MainPageDecktop.png';
-import MainPageDecktop from '../img/MainPageDecktop.png';
+
+import WaterMainPage from '../img/BgMainPage/WaterMainPage.svg';
+import WaterMainTablet from '../img/BgMainPage/WaterMainTablet.svg';
+import WaterMainMobile from '../img/BgMainPage/WaterMainMobile.svg';
+
 import BackgroundMobileDrops from '../img/BackgroundHomePage/BackgroundMobileDrops.png';
 import BackgroundTabletDrops from '../img/BackgroundHomePage/BackgroundTabletDrops.png';
 import BackgroundDesktopDrops from '../img/BackgroundHomePage/BackgroundDesktopDrops.png';
@@ -10,29 +13,35 @@ export const Container = styled.div`
   flex-direction: column;
   width: 100%;
   height: 100%;
-  margin: 0 auto;
+  margin: 0;
   padding: 0;
 `;
 export const WrapHeader = styled.div`
   display: flex;
   width: 100%;
-  height: 50px;
-  margin: 0 auto;
+  height: 10vh;
+  margin: 0;
   padding: 0;
   background-color: #e3f2fd;
 `;
 export const WrapRender = styled.div`
   display: flex;
-  width: 100%;
-  height: 100vh;
-  margin: 0 auto;
+  width: 100vw;
+  height: 90vh;
+  margin: 0;
   padding: 0;
-
-  background-image: url(${MainPageDecktop});
-  background-size: 1440px 432px;
+  background-image: url(${WaterMainMobile});
   background-repeat: no-repeat;
   background-position: bottom;
-  object-fit: cover;
+  background-size: cover;
+
+  @media only screen and (min-width: 768px) {
+    background-image: url(${WaterMainTablet});
+  }
+  @media only screen and (min-width: 1200px) {
+    background-image: url(${WaterMainPage});
+    // background-size: 100vw;
+  }
 `;
 
 export const WrapperHomePage = styled.div`
