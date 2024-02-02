cc6a9e4d4bbd28ff733df3ea26e0ecb9eaa763b1
Roman <jrj80mail@gmail.com>
2024-02-03 00:32:13 +0200

    +WaterDelModal

---
+++ src/components/WaterDelModal/WaterDelModal.styled.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const ModalWrap = styled.div`
+  padding: 0 24px 32px;
+`;
+
+export const ButtonBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+export const Button = styled.button`
+  display: inline-block;
+  width: 100%;
+  padding: 8px 30px;
+  color: #fff;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 24px;
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
+export const TextStyle = styled.p`
+  font-size: 18px;
+  line-height: 20px;
+  font-weight: 500;
+
+  margin-bottom: 24px;
+`;
