c90b7fed3515be19fc19182d7a69d473d8ecd1fb
Iryna_Ptashka <bird.forest70@gmail.com>
2024-02-02 15:05:50 +0200

    deleted ref

--- src/components/Header/UserLogo/UserLogo.jsx
+++ src/components/Header/UserLogo/UserLogo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../../../redux/auth/selectors';
 import UserLogoModal from 'components/ModalUser/UserLogoModal';
