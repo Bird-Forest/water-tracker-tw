edb7f675550745fbfeec21212d57ae8de5be4efd
Motkova Kateryna <sirenkoekaterina90@gmail.com>
2024-02-04 20:57:32 +0200

    fix popup

--- src/components/SectionTodayMonth/Calendar/Calendar.styled.js
+++ src/components/SectionTodayMonth/Calendar/Calendar.styled.js
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
 import theme from '../../../BasicStyle/themeJSX.jsx';
 
-
 export const StyledMonthStatsTable = styled.div`
-display: flex;
-flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 export const MonthNav = styled.div`
   display: flex;
   margin-bottom: 16px;
@@ -36,7 +35,7 @@ export const Button = styled.button`
   .nav {
     width: 14px;
     height: 14px;
-    fill:${theme.colors.primaryAccent};
+    fill: ${theme.colors.primaryAccent};
   }
 
   &:disabled {
@@ -45,28 +44,28 @@ export const Button = styled.button`
   }
 `;
 
-
 export const DaysContainer = styled.div`
-position: relative;
-width: 100%;
+  position: relative;
+  width: 100%;
 
-display: flex;
-flex-wrap: wrap;
+  display: flex;
+  flex-wrap: wrap;
 
-column-gap: 26px;
-row-gap: 16px;
+  column-gap: 26px;
+  row-gap: 16px;
 
-@media screen and (min-width: 768px) {
-  column-gap: 34px;
-  row-gap: 20px;
-  column-gap: 34px;
-}
-@media screen and (min-width: 1440px) {
-  column-gap: 22px;
-}
+  @media screen and (min-width: 768px) {
+    column-gap: 34px;
+    row-gap: 20px;
+    column-gap: 34px;
+  }
+  @media screen and (min-width: 1440px) {
+    column-gap: 22px;
+  }
 `;
 
 export const DayCell = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -84,39 +83,37 @@ export const Popup = styled.div`
   transform: translate(0, -100%);
   transition: all 0.2s ease-in-out;
 
-background-color:${theme.colors.primaryLight};
-border-radius: 10px;
-box-shadow: 0px 4px 4px 0px rgba(64, 123, 255, 0.3);
-padding: 24px 13px;
-z-index: 2;
+  background-color: ${theme.colors.primaryLight};
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px 0px rgba(64, 123, 255, 0.3);
+  padding: 24px 13px;
+  z-index: 2;
 
-@media screen and (max-width: 768px) {
-  position: fixed; 
-   top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  @media screen and (max-width: 768px) {
+    position: fixed;
+    left: ${props => props.x - 140}px;
+    top: ${props => props.y - 35}px;
   }
-  
-@media screen and (min-width: 768px) {
-  padding: 24px 16px;
-  width: 292px;
-}
 
-@media screen and (min-width: 1440px) {
- 
-}
+  @media screen and (min-width: 768px) {
+    padding: 24px 16px;
+    width: 292px;
+  }
 
-h3 {
-  color: ${theme.colors.primaryAccent};
-  font-size: 16px;
-  line-height: 1.25;
-  margin: 0;
-}
-p {
-  color: ${theme.colors.primaryDark};
-  font-size: 16px;
-  line-height: 1.25;
-}
+  @media screen and (min-width: 1440px) {
+  }
+
+  h3 {
+    color: ${theme.colors.primaryAccent};
+    font-size: 16px;
+    line-height: 1.25;
+    margin: 0;
+  }
+  p {
+    color: ${theme.colors.primaryDark};
+    font-size: 16px;
+    line-height: 1.25;
+  }
   span {
     color: ${theme.colors.primaryAccent};
     font-size: 18px;
@@ -126,47 +123,45 @@ p {
 `;
 
 export const Day = styled.div`
-position: relative;
-width: 32px;
-height: 32px;
-border-radius: 50%;
-background-color: ${theme.colors.primaryLight};
-display: flex;
-align-items: center;
-justify-content: center;
-outline: ${props =>
-  props.$isOutlineVisible ? `1px solid ${theme.colors.secondaryYellow}` : 'none'};
-cursor: pointer;
-font-size: 14px;
-line-height: 18px;
-
-&:hover ${Popup} {
+  position: relative;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  background-color: ${theme.colors.primaryLight};
   display: flex;
-}
+  align-items: center;
+  justify-content: center;
+  outline: ${props =>
+    props.$isOutlineVisible
+      ? `1px solid ${theme.colors.secondaryYellow}`
+      : 'none'};
+  cursor: pointer;
+  font-size: 14px;
+  line-height: 18px;
 
-@media (min-width: 768px) {
-  &:hover ${Popup}.right {
-    left: auto;
-    right: 100%;
-    top: -10%
+  &:hover ${Popup} {
+    display: flex;
   }
 
-  &:hover ${Popup}.left {
-    left: 100%;
-    right: auto;
-    top: -10%
+  @media (min-width: 768px) {
+    &:hover ${Popup}.right {
+      left: auto;
+      right: 100%;
+      top: -10%;
+    }
+
+    &:hover ${Popup}.left {
+      left: 100%;
+      right: auto;
+      top: -10%;
+    }
   }
-}
-
-
-
-
-@media screen and (min-width: 768px) {
-  gap: 34px;
-  row-gap: 20px;
-  width: 34px;
-  height: 34px;
-}
+  @media screen and (min-width: 768px) {
+    gap: 34px;
+    row-gap: 20px;
+    width: 34px;
+    height: 34px;
+  }
 `;
 
 export const DayPercent = styled.span`
@@ -188,15 +183,13 @@ export const DayPercent = styled.span`
   }
 `;
 
-
-
 export const StyledMonthWrapper = styled.div`
-color: ${theme.colors.primaryAccent};
-display: flex;
-align-items: center;
-justify-content: space-between;
-min-width: 170px;
-`
+  color: ${theme.colors.primaryAccent};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-width: 170px;
+`;
 export const Month = styled.span`
   font-size: 16px;
   line-height: 20px;
