35c85fdc482662d3f52e6975326b668af6f86aba
Iryna_Ptashka <bird.forest70@gmail.com>
2024-02-05 22:21:04 +0200

    styles welcome

--- src/components/WelcomePage/Main.jsx
+++ src/components/WelcomePage/Main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { TitleMain, WrapMain, WrapMainContent } from './Welcome.styled';
+import { WrapMain, WrapMainContent } from './Welcome.styled';
 import WaterСonsumptionTracker from './WaterСonsumptionTracker';
 import WhyDrinkWater from './WhyDrinkWater';
 
 export default function Main() {
   return (
     <WrapMain>
-      <TitleMain>Water consumption tracker</TitleMain>
       <WrapMainContent>
         <WaterСonsumptionTracker />
         <WhyDrinkWater />
