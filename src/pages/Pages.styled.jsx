f1e5493d35229d2d1df4bc4b289c4d654ca1478c
Iryna_Ptashka <bird.forest70@gmail.com>
2024-02-06 12:02:39 +0200

    background

--- src/pages/Pages.styled.jsx
+++ src/pages/Pages.styled.jsx
@@ -6,7 +6,8 @@ import WaterMainMobile from '../img/BgMainPage/WaterMainMobile.svg';
 
 import bg_mob from '../img/bg/mob-bg.svg';
 import bg_tab from '../img/bg/tab-bg.svg';
-import bg_desk from '../img/bg/desk-bg.svg';
+// import bg_desk from '../img/bg/desk-bg.svg';
+import bg_desk_elem from '../img/bg/desk-elem-bg.svg';
 
 export const Container = styled.div`
   display: flex;
@@ -15,34 +16,46 @@ export const Container = styled.div`
   height: 100%;
   margin: 0;
   padding: 0;
-
-  @media screen and (min-width: 320px) {
-    background-image: url(${bg_mob});
-    background-repeat: no-repeat;
-    background-position-x: center;
-    background-position-y: 108px;
-    background-size: 100%;
-  }
-
-  @media screen and (min-width: 768px) {
-    background-image: url(${bg_tab});
-    background-position-y: 90px;
-  }
-
-  @media screen and (min-width: 1440px) {
-    background-image: url(${bg_desk});
-    background-position-y: center;
-  }
 `;
 export const WrapHeader = styled.div`
   display: flex;
   width: 100%;
-  height: 7vh;
+  height: 8vh;
   margin: 0;
   padding: 0;
-  // background-color: #e3f2fd;
 `;
+
 export const WrapRender = styled.div`
+  display: flex;
+  width: 100%;
+  height: 92vh;
+  margin: 0;
+  padding: 0;
+  background-position: top;
+  background-image: url(${bg_desk_elem});
+
+  @media screen and (min-width: 320px) {
+    // background-image: url(${bg_mob});
+
+    /* background-position-x: center;
+    background-position-y: 108px; */
+    /* background-size: cover; */
+  }
+
+  @media screen and (min-width: 768px) {
+    // background-image: url(${bg_tab});
+    /* background-position-y: 90px; */
+    /* background-repeat: no-repeat;
+     */
+  }
+
+  @media screen and (min-width: 1200px) {
+    // background-image: url(${bg_desk_elem});
+    /* background-position-y: center; */
+    // background-repeat: repeat;
+  }
+`;
+export const WrapWelcomePage = styled.div`
   display: flex;
   width: 100vw;
   height: 93vh;
