16b4c10a4e1064688a9ee90a2f59fb41a6e8e1d2
Yuliia Liubenka <yuliialiubenka@gmail.com>
2024-02-07 17:44:09 +0200

    Fix Routs

--- src/X_files/RestrictedRoute.js
+++ src/X_files/RestrictedRoute.js
@@ -1,12 +1,12 @@
 import { useAuth } from '../hooks';
 import { Navigate } from 'react-router-dom';
 
-// export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
-//   const { isLoggedIn } = useAuth();
-//   return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
-// };
-
-export const RestrictedRoute = ({ children, redirectTo = '/tasks' }) => {
+export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
   const { isLoggedIn } = useAuth();
-  return isLoggedIn ? <Navigate to={redirectTo} replace /> : children;
+  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
 };
+
+// export const RestrictedRoute = ({ children, redirectTo = '/tasks' }) => {
+//   const { isLoggedIn } = useAuth();
+//   return isLoggedIn ? <Navigate to={redirectTo} replace /> : children;
+// };
